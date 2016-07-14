c19749d28bcb4b62ad1fbf209f558b1f14599492
Mike Bel <MikeBelLaptop@Mike-Bel-Laptop.local>
2016-07-14 10:42:19 -0400

    integer section added

--- 1.00_integers.js
+++ 1.00_integers.js
@@ -1,1 +1,51 @@
-bla bla bla test
+// Intergers, their operators/ways to modify/store
+
+var x = 4;
+var crazyNumber = 8;
+console.log(x, crazyNumber);
+
+var newCrazyNumber = x + crazyNumber;
+console.log(x, crazyNumber, newCrazyNumber);
+
+
+var a = 7;
+var b = 5;
+
+var c = a + b;
+
+// or, the basics:
+
+a - b;
+a * b;
+a / b;
+
+// modulus - gives the remainder of division
+a % b; 
+
+// shorthand to dry up code/type less.
+
+//instead of:
+
+crazyNumber = crazyNumber + 5;
+
+//combine operators
+
+crazyNumber += 5;
+crazyNumber -= 3;
+crazyNumber *= 7;
+crazyNumber /= 9;
+
+console.log(crazyNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
