231a4228d26bc4bb8475e5d634fb5914b24013e1
Mike Bel <MikeBelLaptop@Mike-Bel-Laptop.local>
2016-07-16 17:04:42 -0400

    adds loop iteration description

---
+++ 1.71_loop_over_arrays.js
@@ -0,0 +1,10 @@
+// looping, or itering over an array
+// use for loop to 
+
+var smellyThings = ["dead fish", "spoiled milk", "burning dog crap"];
+
+for (i = 0; i < smellyThings.length; i++) {
+	console.log(smellyThings[i]);
+}
+
+
