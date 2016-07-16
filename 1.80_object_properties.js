6ab4e1333caf3f37904329ccdd6737320a2fe833
Mike Bel <MikeBelLaptop@Mike-Bel-Laptop.local>
2016-07-16 17:43:04 -0400

    adds object description

---
+++ 1.80_object_properties.js
@@ -0,0 +1,31 @@
+// objects & their properties and methods
+
+// objects are conceptually similar to tangible, material things in the physical world, 
+// like a car, a dog, a set of blueprints, or a laptop.  they have properties that can be described by
+// key-value pairs (brand : "apple", size: 15, color : "grey" ).  
+
+
+// the key can be any string, and the value any number, string, array, boolean, another object, or a function 
+// (but called a method when it's a value of an object).
+
+
+// object syntax:
+
+
+var computer = {
+
+	//properties
+	formFactor: 		"laptop",
+	brand: 				"apple",
+	size: 				"15",
+	material: 			"aluminum"
+
+};
+
+// to reference/use values of an object, use . syntax.
+
+console.log(computer.brand);
+console.log(computer.material);
+
+
+
